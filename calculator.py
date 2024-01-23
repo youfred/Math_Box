33725a3d2fe28b927681b2c07a189b7db0106545
Sang Beom <youfred@naver.com>
2024-01-23 11:42:05 +0900

    Change License from free to premium

--- calculator.py
+++ calculator.py
@@ -1,5 +1,8 @@
-# 기본 계산기
-def add(a, b):
-  return a+b
-def subtract(a, b):
-  return a-b
+# 기본 계산기 
+
+def add(a ,b):
+	return a+b
+def substract (a,b):
+        return a-b
+def divide(a ,b):
+        return a/b
